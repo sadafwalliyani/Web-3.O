fdabce45f67240098d85a4631559bf5e16670e56
sadafwalliyani <sadafwalliyani@gmail.com>
2023-04-15 22:39:19 +0500

    assignment

--- Java-Assignments/Assignment-4/app.js
+++ Java-Assignments/Assignment-4/app.js
@@ -1,36 +1,36 @@
 // IIFE Immediately invoked function Expressions
 (async function () {
   const response = await fetch("./data.json");
-  const recipes = await response.json();
+  const data = await response.json();
 
   const inputElem = document.getElementById("searchInput");
   const btnElem = document.getElementById("searchBtn");
-  const listElem = document.getElementById("recipe-list");
-  const detailsElem = document.getElementById("recipeDetailsContainer");
+  const listElem = document.getElementById("data-list");
+  const detailsElem = document.getElementById("dataDetailsContainer");
 
-  function loadRecipeDetails(recipe) {
+  function loaddataDetails(data) {
     detailsElem.innerHTML = `
-        <h2 class="title">${recipe.title}</h2>
+        <h2 class="title">${data.title}</h2>
         <h3>Ingredients:</h3>
-        <ul>${recipe.ingredients.map(function (ingredient) {
-          return "<li>" + ingredient + "</li>"
+        <ul>${genres.map(function (genres) {
+          return "<li>" + genres + "</li>"
         }).join("")}</ul>
         <h3>Instruction:</h3>
-        <div>${recipe.instructions}</div>
+        <div>${id.instructions}</div>
     `;
   }
 
   function displaySearchResults (results) {
     listElem.innerHTML = "";
-    results.forEach(function (recipe) {
+    results.forEach(function (data) {
       const li = document.createElement("li");
       const listItem = `
-          <h2 class="title">${recipe.title}</h2>
-          <div class="description">${recipe.description}</div>
+          <h2 class="title">${data.vote_average}</h2>
+          <div class="description">${data.description}</div>
       `;
       li.innerHTML = listItem;
       li.addEventListener("click", function () {
-        loadRecipeDetails(recipe);
+        loaddataDetails(data);
       });
       listElem.appendChild(li);
     })
@@ -38,9 +38,9 @@
 
   function search() {
     const query = inputElem.value.toLowerCase();
-    const results = recipes.filter(function (recipe) {
-      return (recipe.title.toLowerCase().includes(query) ||
-      recipe.ingredients.join(" ").toLowerCase().includes(query))
+    const results = data.filter(function (data) {
+      return (data.title.toLowerCase().includes(query) ||
+      data.id.join(" ").toLowerCase().includes(query))
     });
 
     displaySearchResults(results);
